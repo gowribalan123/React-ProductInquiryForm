6b8e92b0f53ca2e6b19b28842f5832f1136c649e
gowribalan123 <gowribalanak@gmail.com>
2024-11-03 06:44:25 +0530

    First commit

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
      
       
       <p>
-      <span class="enq">EnquiryMart</span> is one of the leading online platforms of trusted and verified packers and movers in India. Our main objective is to help our customers in finding the best and reliable relocation service providers. We assist you in finding the best packers and movers service provider in India, providing the best quality services.
+      <span class="enq">InquiryMart</span> is one of the leading online platforms of trusted and verified packers and movers in India. Our main objective is to help our customers in finding the best and reliable relocation service providers. We assist you in finding the best packers and movers service provider in India, providing the best quality services.
       </p>
       <p>
       InquiryMart offers you an online search portal to locate the Best logistics companies near your area who can assist you in finding the right relocation service provider for your office, home, commercial and industrial requirements. We aim to make relocation comfortable and secure. Our packers and movers service providers are working in the diversified areas like Local and domestic moving, International moving, cargo services, pet moving as well.
